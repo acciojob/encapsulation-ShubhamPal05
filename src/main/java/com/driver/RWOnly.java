d7e266f67fa21ffafacc25c54448b9ca34169f66
unknown <ShubhamPal05>
2022-10-09 19:50:23 +0530

    second try

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+    String test;
+
+    public String getName() {
+        return this.name;
+    }
     
+    public String setName(String name){
+        this.name=name;
+        return this.name;
+    }
 }
